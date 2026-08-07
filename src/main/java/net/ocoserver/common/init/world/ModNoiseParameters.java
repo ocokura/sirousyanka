4508b51710d86047ba57228a0c51fe5f9823551d
ocokura <ocokura@gmail.com>
2026-08-07 22:42:17 +0900

    山の形を決定

--- src/main/java/net/ocoserver/common/init/world/ModNoiseParameters.java
+++ src/main/java/net/ocoserver/common/init/world/ModNoiseParameters.java
@@ -9,13 +9,15 @@ import net.ocoserver.common.util.ModUtils;
 public class ModNoiseParameters {
 
     public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_BASE_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_base_noise");
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_mountain_noise");
+    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_BASE_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_base_mountain_noise");
+    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_SMALL_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_small_mountain_noise");
     public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_PEAK_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_peak_noise");
     public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_SMALL_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_small_noise");
 
     public static void bootstrap(BootstrapContext<NormalNoise.NoiseParameters> context) {
         context.register(FROZEN_ABYSS_BASE_NOISE, new NormalNoise.NoiseParameters(-5, 2, 0.5));
-        context.register(FROZEN_ABYSS_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-7, 3));
+        context.register(FROZEN_ABYSS_BASE_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-8, 3));
+        context.register(FROZEN_ABYSS_SMALL_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-3, 1));
         context.register(FROZEN_ABYSS_PEAK_NOISE, new NormalNoise.NoiseParameters(-4, 2));
         context.register(FROZEN_ABYSS_SMALL_NOISE, new NormalNoise.NoiseParameters(-3, 1));
     }
--- src/main/java/net/ocoserver/common/worldgen/dimension/FrozenAbyssNoise.java
+++ src/main/java/net/ocoserver/common/worldgen/dimension/FrozenAbyssNoise.java
@@ -18,17 +18,12 @@ public class FrozenAbyssNoise {
         DensityFunction smallNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.28, noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_SMALL_NOISE));
         DensityFunction plainNoise = DensityFunctions.add(DensityFunctions.mul(baseNoise, DensityFunctions.constant(0.65)), DensityFunctions.mul(smallNoise, DensityFunctions.constant(0.35)));
 
-        DensityFunction mountainBaseNoise =
-                DensityFunctions.shiftedNoise2d(
-                        shiftX,
-                        shiftZ,
-                        0.33,
-                        noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_MOUNTAIN_NOISE)
-                );
+        DensityFunction mountainBaseNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.2, noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_BASE_MOUNTAIN_NOISE));
+        DensityFunction mountainSmallNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_BASE_MOUNTAIN_NOISE));
 
-        DensityFunction mountainNoise = DensityFunctions.add(DensityFunctions.mul(mountainBaseNoise, DensityFunctions.constant(0.9)), DensityFunctions.mul(smallNoise, DensityFunctions.constant(0.1)));
+        DensityFunction mountainNoise = DensityFunctions.add(DensityFunctions.mul(mountainBaseNoise, DensityFunctions.constant(0.61)), DensityFunctions.mul(mountainSmallNoise, DensityFunctions.constant(0.39)));
 
-        DensityFunction mountain = DensityFunctions.mul(DensityFunctions.add(mountainNoise, DensityFunctions.constant(-0.5)).clamp(0, 1), DensityFunctions.constant(50));
+        DensityFunction mountain = DensityFunctions.mul(DensityFunctions.add(mountainNoise, DensityFunctions.constant(-0.47)).clamp(0, 1), DensityFunctions.constant(50));
 
         DensityFunction height = DensityFunctions.mul(plainNoise, DensityFunctions.constant(5));
         height = DensityFunctions.add(height, mountain);
