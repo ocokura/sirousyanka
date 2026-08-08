be6d0ecec6262caf3cae1f0069c6e18addb7ebcc
ocokura <ocokura@gmail.com>
2026-08-08 20:46:31 +0900

    雪山の形を大まかに決定

--- src/main/java/net/ocoserver/common/init/world/ModNoiseParameters.java
+++ src/main/java/net/ocoserver/common/init/world/ModNoiseParameters.java
@@ -8,17 +8,22 @@ import net.ocoserver.common.util.ModUtils;
 
 public class ModNoiseParameters {
 
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_BASE_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_base_noise");
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_BASE_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_base_mountain_noise");
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_SMALL_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_small_mountain_noise");
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_PEAK_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_peak_noise");
-    public static final ResourceKey<NormalNoise.NoiseParameters> FROZEN_ABYSS_SMALL_NOISE = ModUtils.getResourceKey(Registries.NOISE, "frozen_abyss_small_noise");
+    public static final ResourceKey<NormalNoise.NoiseParameters> BASE_NOISE = ModUtils.getResourceKey(Registries.NOISE, "base_noise");
+    public static final ResourceKey<NormalNoise.NoiseParameters> SMALL_NOISE = ModUtils.getResourceKey(Registries.NOISE, "small_noise");
+
+    public static final ResourceKey<NormalNoise.NoiseParameters> BASE_MOUNTAIN_NOISE = ModUtils.getResourceKey(Registries.NOISE, "base_mountain_noise");
+
+    public static final ResourceKey<NormalNoise.NoiseParameters> BASE_PEAK_NOISE = ModUtils.getResourceKey(Registries.NOISE, "base_peak_noise");
+    public static final ResourceKey<NormalNoise.NoiseParameters> SMALL_PEAK_NOISE = ModUtils.getResourceKey(Registries.NOISE, "small_peak_noise");
+
 
     public static void bootstrap(BootstrapContext<NormalNoise.NoiseParameters> context) {
-        context.register(FROZEN_ABYSS_BASE_NOISE, new NormalNoise.NoiseParameters(-5, 2, 0.5));
-        context.register(FROZEN_ABYSS_BASE_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-8, 3));
-        context.register(FROZEN_ABYSS_SMALL_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-3, 1));
-        context.register(FROZEN_ABYSS_PEAK_NOISE, new NormalNoise.NoiseParameters(-4, 2));
-        context.register(FROZEN_ABYSS_SMALL_NOISE, new NormalNoise.NoiseParameters(-3, 1));
+        context.register(BASE_NOISE, new NormalNoise.NoiseParameters(-5, 2, 0.5));
+        context.register(SMALL_NOISE, new NormalNoise.NoiseParameters(-3, 1));
+
+        context.register(BASE_MOUNTAIN_NOISE, new NormalNoise.NoiseParameters(-8, 3));
+
+        context.register(BASE_PEAK_NOISE, new NormalNoise.NoiseParameters(-7, 3));
+        context.register(SMALL_PEAK_NOISE, new NormalNoise.NoiseParameters(-3, 1));
     }
 }
--- src/main/java/net/ocoserver/common/worldgen/dimension/FrozenAbyssNoise.java
+++ src/main/java/net/ocoserver/common/worldgen/dimension/FrozenAbyssNoise.java
@@ -14,19 +14,38 @@ public class FrozenAbyssNoise {
         DensityFunction shiftX = new DensityFunctions.HolderHolder(density.getOrThrow(NoiseRouterAccessor.shiftX()));
         DensityFunction shiftZ = new DensityFunctions.HolderHolder(density.getOrThrow(NoiseRouterAccessor.shiftZ()));
 
-        DensityFunction baseNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_BASE_NOISE));
-        DensityFunction smallNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.28, noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_SMALL_NOISE));
+        //平原
+        DensityFunction baseNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.BASE_NOISE));
+        DensityFunction smallNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.28, noises.getOrThrow(ModNoiseParameters.SMALL_NOISE));
         DensityFunction plainNoise = DensityFunctions.add(DensityFunctions.mul(baseNoise, DensityFunctions.constant(0.65)), DensityFunctions.mul(smallNoise, DensityFunctions.constant(0.35)));
 
-        DensityFunction mountainBaseNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.2, noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_BASE_MOUNTAIN_NOISE));
-        DensityFunction mountainSmallNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.FROZEN_ABYSS_BASE_MOUNTAIN_NOISE));
-
+        //山
+        DensityFunction mountainBaseNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.2, noises.getOrThrow(ModNoiseParameters.BASE_MOUNTAIN_NOISE));
+        DensityFunction mountainSmallNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.BASE_MOUNTAIN_NOISE));
         DensityFunction mountainNoise = DensityFunctions.add(DensityFunctions.mul(mountainBaseNoise, DensityFunctions.constant(0.61)), DensityFunctions.mul(mountainSmallNoise, DensityFunctions.constant(0.39)));
 
-        DensityFunction mountain = DensityFunctions.mul(DensityFunctions.add(mountainNoise, DensityFunctions.constant(-0.47)).clamp(0, 1), DensityFunctions.constant(50));
+        //雪山
+        DensityFunction peakBaseNoise = DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.2, noises.getOrThrow(ModNoiseParameters.BASE_PEAK_NOISE));
+        DensityFunction peakSmallNoise = DensityFunctions.noise(noises.getOrThrow(ModNoiseParameters.SMALL_PEAK_NOISE));
+        DensityFunction peakNoise = DensityFunctions.add(DensityFunctions.mul(peakBaseNoise, DensityFunctions.constant(0.9)), DensityFunctions.mul(peakSmallNoise, DensityFunctions.constant(0.1)));
+
+        //合成
+        DensityFunction mountain = DensityFunctions.mul(DensityFunctions.add(mountainNoise, DensityFunctions.constant(-0.27)).clamp(0, 1), DensityFunctions.constant(50));
+        DensityFunction peak =
+                DensityFunctions.mul(
+                        DensityFunctions.mul(
+                                DensityFunctions.add(
+                                        mountainNoise,
+                                        DensityFunctions.constant(-0.8)
+                                ).clamp(0, 1),
+                                peakNoise.clamp(0, 1)
+                        ),
+                        DensityFunctions.constant(200)
+                );
 
         DensityFunction height = DensityFunctions.mul(plainNoise, DensityFunctions.constant(5));
         height = DensityFunctions.add(height, mountain);
+        height = DensityFunctions.add(height, peak);
 
         return DensityFunctions.add(height, DensityFunctions.constant(60));
     }
